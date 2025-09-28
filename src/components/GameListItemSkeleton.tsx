04f076ed015a43ea1b4ebea9b6fce241031ea55e
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-09-28 17:08:31 +0800

    Game list item skeleton

---
+++ src/components/GameListItemSkeleton.tsx
@@ -0,0 +1,9 @@
+export default function GameListItemSkeleton() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="skeleton h-[200px] w-full"></div>
+      <div className="skeleton h-4 w-[1/2]"></div>
+      <div className="skeleton h-4 w-full"></div>
+    </div>
+  );
+}
