3707bc583a6c365286296bd5403acc0f1fe8e3ce
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-09-28 09:15:00 +0800

    Create responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
 export default function App() {
-  return <button className="btn btn-primary">Default</button>;
+  return (
+    <div>
+      <header className="bg-red-100">
+        <nav className="container mx-auto bg-red-500 p-4">Nav</nav>
+      </header>
+
+      <div className="container mx-auto flex">
+        <aside className="bg-green-500 w-[250px] hidden lg:block p-4">
+          Aside
+        </aside>
+        <main className="bg-blue-500 flex-1 p-4">Main</main>
+      </div>
+    </div>
+  );
 }
