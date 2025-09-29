c912d30a27ec1adadbea9961daa5ce3f0a786981
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-09-29 11:24:05 +0800

    No games found in game list

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -7,6 +7,10 @@ export default function GameList() {
   const { data, isLoading, error } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if (data?.count === 0) {
+    return <p>No games found</p>;
+  }
+
   if (error) {
     return <p>{error.message}</p>;
   }
