aeaa85ad869e495086fc84d4266205ec39afe1f2
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-10-18 22:50:11 +0800

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <header className="bg-red-500">Header</header>
+      <div className="flex">
+        <aside className="w-[240px] hidden lg:block bg-green-500">Aside</aside>
+        <main className="flex-1 bg-blue-500">Main</main>
+      </div>
+    </div>
+  );
 }
