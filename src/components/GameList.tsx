0dc30a3fe5f6cc690cf21b6ea987a2844f5bcd47
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-10-19 09:46:04 +0800

    Game list item skeleton

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,9 +1,11 @@
 import GameListItem from "@/components/GameListItem";
+import GameListItemSkeleton from "@/components/GameListItemSkeleton";
 
 import useGames from "@/hooks/useGames";
 
 export default function GameList() {
   const { data, isLoading, error } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   if (error) {
     return <p>{error.message}</p>;
@@ -11,6 +13,9 @@ export default function GameList() {
 
   return (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {isLoading &&
+        skeletons.map((skeleton) => <GameListItemSkeleton key={skeleton} />)}
+
       {data?.results.map((game) => (
         <GameListItem key={game.id} game={game} />
       ))}
--- src/components/GameListItemSkeleton.tsx
+++ src/components/GameListItemSkeleton.tsx
@@ -1,3 +1,9 @@
 export default function GameListItemSkeleton() {
-  return <div></div>;
+  return (
+    <li className="shadow-sm space-y-4">
+      <div className="skeleton h-60 w-full"></div>
+      <div className="skeleton h-4 w-full"></div>
+      <div className="skeleton h-4 w-1/2"></div>
+    </li>
+  );
 }
