1f5cddad7f32f0c353a1d7d7f2e86c4c656a5dd9
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-10-19 22:38:27 +0800

    Update game details page padding

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -18,13 +18,13 @@ export default function GameDetailsPage() {
 
   return (
     <main className="flex flex-col lg:flex-row gap-8 mt-4">
-      <section className="flex-1 p-2 space-y-8">
+      <section className="flex-1 p-4 space-y-8">
         <h1 className="text-3xl font-bold">{data.name}</h1>
         <ExpandableText>{data.description_raw}</ExpandableText>
         <AttributesList game={data} />
       </section>
 
-      <section className="flex-1 p-2 space-y-4">
+      <section className="flex-1 p-4 space-y-4">
         <Movie id={data.id} />
         <Screenshots id={data.id} />
       </section>
