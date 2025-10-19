7b46079a06fa15ee0b12210ae53fa034d0270092
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-10-19 17:43:24 +0800

    Logo link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 import SearchInput from "@/components/SearchInput";
 import ThemeSwitch from "@/components/ThemeSwitch";
 
@@ -6,7 +8,9 @@ import logoImg from "@/assets/images/logo.webp";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center gap-2 md:gap-4 py-2 pr-2">
-      <img src={logoImg} alt="Game Hub logo" className="w-16 aspect-square" />
+      <Link to="/" className="shrink-0">
+        <img src={logoImg} alt="Game Hub logo" className="w-16 aspect-square" />
+      </Link>
 
       <SearchInput />
 
