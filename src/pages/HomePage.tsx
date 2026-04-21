2761859dccfe6e64ad56d9338c052e5bcdef13dd
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-04-21 16:13:25 +0800

    Responsive layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <main className="grid grid-cols-12">
+      <aside className="hidden md:col-span-3 md:block">Aside</aside>
+
+      <section className="md:col-span-9">Section</section>
+    </main>
+  )
 }
