57ea2abc8fd748bf0b6f15fc8f73f93cd7139971
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-04-21 16:09:52 +0800

    Setup tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,15 @@ import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { RouterProvider } from "react-router/dom"
 import router from "@/lib/router"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 )
