538eb582c2f7af66842cbf12817d81b1e99b3b1f
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-04-21 15:49:49 +0800

    Navbar logo link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router"
+
 import Container from "@/components/Container"
 import SearchGames from "@/features/game/components/SearchGames"
 import ThemeSwitcher from "@/components/ThemeSwitcher"
@@ -8,7 +10,9 @@ export default function Navbar() {
   return (
     <nav className="py-4">
       <Container className="flex flex-col items-center gap-4 sm:flex-row">
-        <img src={logoImg} alt="Game Hub logo" className="size-16" />
+        <Link to="/">
+          <img src={logoImg} alt="Game Hub logo" className="size-16" />
+        </Link>
 
         <SearchGames />
 
